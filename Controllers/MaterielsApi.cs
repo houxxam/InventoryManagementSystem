47f4d32ea485535762151db8ac5ee74d31fde1bf
houxxam <houxxam95@gmail.com>
2024-02-19 22:33:24 +0100

    fix api

--- Controllers/MaterielsApi.cs
+++ Controllers/MaterielsApi.cs
@@ -1,5 +1,6 @@
 ﻿using InvWebApp.Data;
 using InvWebApp.Models;
+using InvWebApp.Models.Dtos;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -34,12 +35,12 @@ namespace InvWebApp
         // POST: api/Materiels
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<Materiel>> PostMateriel(string materielName, string serialNumber, string categorie, string service, string group)
+        public async Task<ActionResult<Materiel>> PostMateriel([FromBody] CreateMaterielDto dto)
         {
             // /api/MaterielsApi?materielName1=&serialNumber=&categorie=&service=&group=
-            var cat = _context.Categories.FirstOrDefault(c => c.CategorieName.ToLower() == categorie.ToLower());
-            var ser = _context.Services.FirstOrDefault(s => s.ServiceName.ToLower() == service.ToLower());
-            var grp = _context.serviceGroups.FirstOrDefault(g => g.GroupName.ToLower() == group.ToLower());
+            var cat = _context.Categories.FirstOrDefault(c => c.CategorieName.ToLower() == dto.Categorie.ToLower());
+            var ser = _context.Services.FirstOrDefault(s => s.ServiceName.ToLower() == dto.Service.ToLower());
+            var grp = _context.serviceGroups.FirstOrDefault(g => g.GroupName.ToLower() == dto.Group.ToLower());
 
 
 
@@ -47,19 +48,19 @@ namespace InvWebApp
 
             if (cat is null)
             {
-                _context.Categories.Add(new() { CategorieName = categorie, UserId = 1 });
+                _context.Categories.Add(new() { CategorieName = dto.Categorie, UserId = 1 });
                 _context.SaveChanges();
                 cat = _context.Categories.OrderByDescending(c => c.Id).First();
             }
             if (ser is null)
             {
-                _context.Services.Add(new() { ServiceName = service, UserId = 1 });
+                _context.Services.Add(new() { ServiceName = dto.Service, UserId = 1 });
                 _context.SaveChanges();
                 ser = _context.Services.OrderByDescending(s => s.Id).First();
             }
             if (grp is null)
             {
-                _context.serviceGroups.Add(new() { GroupName = group, ServiceId = ser.Id });
+                _context.serviceGroups.Add(new() { GroupName = dto.Group, ServiceId = ser.Id });
                 _context.SaveChanges();
                 grp = _context.serviceGroups.OrderByDescending(s => s.Id).First();
             }
@@ -67,9 +68,9 @@ namespace InvWebApp
 
 
             var materiel = new Materiel();
-            materiel.MaterielName = materielName;
+            materiel.MaterielName = dto.MaterielName;
             materiel.CreatedDate = DateTime.Now;
-            materiel.SerialNumber = serialNumber;
+            materiel.SerialNumber = dto.SerialNumber;
             materiel.CategorieId = cat.Id;
             materiel.ServiceId = ser.Id;
             materiel.ServiceGroupId = grp.Id;
