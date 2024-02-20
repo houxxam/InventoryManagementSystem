6726331ce91fd4bacde481ae2c02e97f2ddd2641
houxxam <houxxam95@gmail.com>
2024-02-20 10:07:52 +0100

    api fix check serial number before add

--- Controllers/MaterielsApi.cs
+++ Controllers/MaterielsApi.cs
@@ -37,50 +37,58 @@ namespace InvWebApp
         [HttpPost]
         public async Task<ActionResult<Materiel>> PostMateriel([FromBody] CreateMaterielDto dto)
         {
-            // /api/MaterielsApi?materielName1=&serialNumber=&categorie=&service=&group=
-            var cat = _context.Categories.FirstOrDefault(c => c.CategorieName.ToLower() == dto.Categorie.ToLower());
-            var ser = _context.Services.FirstOrDefault(s => s.ServiceName.ToLower() == dto.Service.ToLower());
-            var grp = _context.serviceGroups.FirstOrDefault(g => g.GroupName.ToLower() == dto.Group.ToLower());
+            var mat = _context.Materiels.FirstOrDefault(s=>s.SerialNumber == dto.SerialNumber);
 
-
-
-
-
-            if (cat is null)
+            if (dto.SerialNumber != mat.SerialNumber)
             {
-                _context.Categories.Add(new() { CategorieName = dto.Categorie, UserId = 1 });
-                _context.SaveChanges();
-                cat = _context.Categories.OrderByDescending(c => c.Id).First();
+                // /api/MaterielsApi?materielName1=&serialNumber=&categorie=&service=&group=
+                var cat = _context.Categories.FirstOrDefault(c => c.CategorieName.ToLower() == dto.Categorie.ToLower());
+                var ser = _context.Services.FirstOrDefault(s => s.ServiceName.ToLower() == dto.Service.ToLower());
+                var grp = _context.serviceGroups.FirstOrDefault(g => g.GroupName.ToLower() == dto.Group.ToLower());
+
+
+
+
+
+                if (cat is null)
+                {
+                    _context.Categories.Add(new() { CategorieName = dto.Categorie, UserId = 1 });
+                    _context.SaveChanges();
+                    cat = _context.Categories.OrderByDescending(c => c.Id).First();
+                }
+                if (ser is null)
+                {
+                    _context.Services.Add(new() { ServiceName = dto.Service, UserId = 1 });
+                    _context.SaveChanges();
+                    ser = _context.Services.OrderByDescending(s => s.Id).First();
+                }
+                if (grp is null)
+                {
+                    _context.serviceGroups.Add(new() { GroupName = dto.Group, ServiceId = ser.Id });
+                    _context.SaveChanges();
+                    grp = _context.serviceGroups.OrderByDescending(s => s.Id).First();
+                }
+
+
+
+                var materiel = new Materiel();
+                materiel.MaterielName = dto.MaterielName;
+                materiel.CreatedDate = DateTime.Now;
+                materiel.SerialNumber = dto.SerialNumber;
+                materiel.CategorieId = cat.Id;
+                materiel.ServiceId = ser.Id;
+                materiel.ServiceGroupId = grp.Id;
+                materiel.UserId = 1;
+                _context.Materiels.Add(materiel);
+                await _context.SaveChangesAsync();
+
+                return CreatedAtAction("GetMateriel", new { id = materiel.Id }, materiel);
             }
-            if (ser is null)
-            {
-                _context.Services.Add(new() { ServiceName = dto.Service, UserId = 1 });
-                _context.SaveChanges();
-                ser = _context.Services.OrderByDescending(s => s.Id).First();
+            else {
+                 return BadRequest("SerialNumber already exist"); 
             }
-            if (grp is null)
-            {
-                _context.serviceGroups.Add(new() { GroupName = dto.Group, ServiceId = ser.Id });
-                _context.SaveChanges();
-                grp = _context.serviceGroups.OrderByDescending(s => s.Id).First();
-            }
-
-
-
-            var materiel = new Materiel();
-            materiel.MaterielName = dto.MaterielName;
-            materiel.CreatedDate = DateTime.Now;
-            materiel.SerialNumber = dto.SerialNumber;
-            materiel.CategorieId = cat.Id;
-            materiel.ServiceId = ser.Id;
-            materiel.ServiceGroupId = grp.Id;
-            materiel.UserId = 1;
-            _context.Materiels.Add(materiel);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetMateriel", new { id = materiel.Id }, materiel);
+        
         }
-
 
 
 
